b08e5e76ce86e53164d6a779443140c2a15264bd
Huzaifa <huzaifajazzmankda@gmail.com>
2019-03-13 12:26:50 +0530

    Bug Fixes

--- dsu.cpp
+++ dsu.cpp
@@ -1,11 +1,20 @@
 //Disjoint Set Union
-long long int par[MAX],connected;
 // DON'T FORGET TO INITIALIZE PARENT
 /*Find parent is post order. i.e. first  reaches
   representative element, then it takes the value
   and while returning it updates corresponding children
   1->2->3->4->5     first it reaches 5 then updates par[4]=5
   then updates par[3]=par[4](which is now 5)*/
+long long int par[MAX],sz[MAX],connected;
+void clr_dsu(int n)
+{
+	for(int i=0;i<MAX;i++)
+	{
+		par[i]=i;
+		sz[i]=0;
+	}
+	connected=n;
+}
 void find_par(long long int k)
 {
 	if(par[par[k]]==k)//reached representative element now update will start
