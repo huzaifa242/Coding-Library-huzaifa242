f4bbd933941d252207067765b5ff0a88d11fdfed
Huzaifa <huzaifajazzmankda@gmail.com>
2019-03-13 18:04:24 +0530

    Initial Commit

---
+++ topological_sort.cpp
@@ -0,0 +1,29 @@
+int idgr[MAX],n,m;
+vector<int> topo,adjlst[MAX];
+bool topological_sort()
+{
+	int i;
+	memset(idgr,0,sizeof(idgr));
+	priority_queue<int, vector<int>, greater<int> > pq;
+	for(i=1;i<=n;i++)
+	{
+		for(auto u:adjlst[i])
+		idgr[u]++;
+	}
+	for(i=1;i<=n;i++)
+	if(!idgr[i])
+	pq.push(i);
+	while(!pq.empty())
+	{
+		auto u=pq.top();
+		pq.pop();
+		topo.push_bac(u);
+		for(auto i:adjlst[u])
+		{
+			idgr[i]--;
+			if(!idgr[i])
+			pq.push(i);
+		}
+	}
+	return topo.size()==n;
+}
