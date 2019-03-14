700fe30a897054f08e7eae1f94fe8c16e8a7c364
Huzaifa <huzaifajazzmankda@gmail.com>
2019-03-14 15:32:44 +0530

    Initial Commit

---
+++ articulation_points.cpp
@@ -0,0 +1,48 @@
+vector<int> adjlst[MAX],artpt;
+int n,vis[MAX],tin[MAX],low[MAX],ptr;
+bool arti[MAX];
+void arti_dfs(int x, int pr)
+{
+	vis[x]=1;
+	tin[x]=ptr;
+	low[x]=ptr;
+	ptr++;
+	int kf=0;
+	for(int i=0;i<adjlst[x].size();i++)
+	{
+		int v=adjlst[x][i];
+		if(v==pr)continue;
+		else if(vis[v])
+		low[x]=min(low[x],tin[v]);
+		else
+		{
+			arti_dfs(v,x);
+			low[x]=min(low[x],low[v]);
+			if(low[v]>=tin[x] && pr!=0)
+			{
+				if(!arti[x])
+				artpt.push_back(x);
+				arti[x]=true;
+			}
+			kf++;
+		}
+	}
+	if(pr==0 && kf>1)
+	{
+		if(!arti[x])
+		artpt.push_back(x);
+		arti[x]=true;
+	}
+}
+void articulation_points()
+{
+	ptr=0;
+	memset(vis,0,sizeof(vis));
+	memset(tin,0,sizeof(tin));
+	memset(low,0,sizeof(low));
+	memset(arti,0,sizeof(arti));
+	artpt.clear();
+	for(int i=1;i<=n;i++)
+	if(!vis[i])
+	arti_dfs(i,0);
+}
---
+++ bridges.cpp
@@ -0,0 +1,38 @@
+vector<int> adjlst[MAX];
+vector<pair<int,int> > bridgpt;
+int n,vis[MAX],tin[MAX],low[MAX],ptr;
+void brdgi_dfs(int x, int pr)
+{
+	vis[x]=1;
+	tin[x]=ptr;
+	low[x]=ptr;
+	ptr++;
+	for(int i=0;i<adjlst[x].size();i++)
+	{
+		int v=adjlst[x][i];
+		if(v==pr)continue;
+		else if(vis[v])
+		low[x]=min(low[x],tin[v]);
+		else
+		{
+			brdgi_dfs(v,x);
+			low[x]=min(low[x],low[v]);
+			if(low[v]>tin[x])
+			{
+				bridgpt.push_back({min(x,v),max(x,v)});
+			}
+		}
+	}
+}
+void bridges()
+{
+	ptr=0;
+	memset(vis,0,sizeof(vis));
+	memset(tin,0,sizeof(tin));
+	memset(low,0,sizeof(low));
+	memset(arti,0,sizeof(arti));
+	bridgpt.clear();
+	for(int i=1;i<=n;i++)
+	if(!vis[i])
+	brdgi_dfs(i,0);
+}
