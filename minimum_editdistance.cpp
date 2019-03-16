a9c25db977c2165c39619752e47d1db3d5b01b39
Huzaifa <huzaifajazzmankda@gmail.com>
2019-03-16 17:38:07 +0530

    Bug Fixes

--- minimum_editdistance.cpp
+++ minimum_editdistance.cpp
@@ -1,4 +1,5 @@
 //Usage min_edit(a,b,in,rm,sb);
+//ci-insertion, cr=deletion, cs-substition
 int cr,ci,cs,min_dp[MAX][MAX];
 int conv_atob(string &a,string &b)
 {
@@ -26,4 +27,62 @@ int min_edit(string &a,string &b, int ins=1, int rmv=1,int sub=2)
 	cr=rmv;
 	cs=sub;
 	return min(conv_atob(a,b),conv_atob(b,a));
+}
+//--------------------------------------------------------------------------------
+//Usage same as above just use for case resticted where 
+// cost cannot increase k
+int cr,ci,cs,mdpp[MAX],mdps[MAX];
+{
+	memset(mdpp,0,sizeof(mdpp));
+	memset(mdps,0,sizeof(mdps));
+	int i,j,n=a.size(),m=b.size();
+	if(ci==0 && cr==0)
+	return 0;
+	if(n<m)
+	{
+		if((m-n)>k/ci)
+		return -1;
+	}
+	if(m<n)
+	{
+		if((n-m)>k/cr)
+		return -1;
+	}
+	for(i=1;i<=m;i++)
+	mdpp[i]=ci*i;
+	for(i=1;i<=n;i++)
+	{
+		mdps[0]=ci*i;
+		for(j=max(1,i-k);j<=min(m,i+k);j++)
+		{
+			if(a[i-1]==b[j-1])
+			mdps[j]=mdpp[j-1];
+			else if(j==max(1,i-k))
+			mdps[j]=min(mdpp[j-1]+cs,mdpp[j]+cr);
+			else if(j==min(m,i+k))
+			mdps[j]=min(mdpp[j-1]+cs,mdps[j-1]+ci);
+			else
+			mdps[j]=min(mdpp[j-1]+cs,min(mdpp[j]+cr,mdps[j-1]+ci));
+		}
+		for(j=max(1,i-k);j<=min(m,i+k);j++)
+		mdpp[j]=mdps[j];
+		mdpp[0]=mdps[0];
+	}
+	return mdps[m]>k?-1:mdps[m];
+}
+int min_edit(string &a,string &b, int ins=1, int rmv=1,int sub=2,int k=5*MAX)
+{
+	ci=ins;
+	cr=rmv;
+	cs=sub;
+	int pp=conv_atob(a,b,k);
+	if(pp==-1)
+	pp=5*MAX;
+	int qq=conv_atob(b,a,k);
+	if(qq==-1)
+	qq=5*MAX;
+	if(pp==5*MAX && qq==5*MAX)
+	return -1;
+	else
+	return min(pp,qq); 
 }
