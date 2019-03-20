5a3fb38703e04c938fffe9bde5d462bf2f95a275
Huzaifa <huzaifajazzmankda@gmail.com>
2019-03-20 16:10:03 +0530

    Bug Fixes

--- in_builtRef.cpp
+++ in_builtRef.cpp
@@ -11,6 +11,7 @@ struct pqcmp
 		//<-asc  >-desc
 	}
 };
+//Double Ended Queue Structure
 deque<T> dq;
 dq.push_back();
 dq.push_front();
@@ -19,19 +20,30 @@ dq.back();
 dq.pop_front();
 dq.pop_back();
 
+//Set
 set<T> s;
 s.insert(10);
 s.erase(10);
 
+//Queue
 queue<T> qu;
 qu.push();
 qu.front();
 qu.back();
 qu.pop();
 
+//Stack
 stack<T> st;
 st.push();
 st.top();
 st.pop();
 
 __gcd(a,b);
+
+//C++ Lambda
+sort(a,a+n,[&](int a, int b){return a>b });
+
+__builtin_popcount(6); //returns 2. Number of set bits in binary of a number
+__builtin_parity(x); //returns 1 for odd parity, 0 for even;
+__builtin_clz(16); //returns 27. 32-pos(MSB 1) 64 for long long int
+__builtin_ctz(16); //returns 4 position of (LSB 1) -1
