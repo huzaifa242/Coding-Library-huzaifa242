07fd84f439c72df6f79ebed80003927c90009fca
Huzaifa <huzaifajazzmankda@gmail.com>
2019-05-25 17:01:43 +0530

    initial commit

---
+++ digit dp.cpp
@@ -0,0 +1,45 @@
+//dp[id][ls][sm] id represents the number of digits
+// ls represents the less flag ls=1 means less
+// sm will be terminating condition on count of particular digit
+//Usage write 3rd parameter of dp. then solve(z), will give count from 0 to z
+int dp[MAX][2][];
+vector<int> digi;
+
+int call(int id, int ls, int sm)
+{
+	// failure of count condition
+	if(sm> ) 
+		return 0;
+	//reached end of all digits
+	if(id==digi.size())
+		return 1;
+	// answer cached
+	if(dp[id][ls][sm]!=-1)
+		return dp[id][ls][sm];
+	int dans=0;
+	if(!ls)
+	{
+		for(int i=0;i<=digi[id];i++)
+		dans+=call(id+1,ls|(i<digi[id]),sm+(i>0));
+	}
+	else
+	{
+		for(int i=0;i<=9;i++)
+		dans+=call(id+1,ls,sm+(i>0));	
+	}
+	return dp[id][ls][sm]=dans;
+}
+
+int solve(int z)
+{
+	digi.clear();
+	memset(dp,-1,sizeof(dp));	
+	int sm=0;
+	while(z)
+	{
+		digi.push_back(z%10LL);
+		z/=10LL;
+	}
+	reverse(digi.begin(),digi.end());
+	return call(0,0,0);
+}
