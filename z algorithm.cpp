e5a64dd23d0f5da5dad98aeb6f770611a037263b
Huzaifa <huzaifajazzmankda@gmail.com>
2019-06-04 16:24:40 +0530

    bug fixes

--- z algorithm.cpp
+++ z algorithm.cpp
@@ -7,7 +7,7 @@ vector<int> z_algo(string s)
 	for (i=1,l=0,r=0; i<s.size();i++)
 	{
 		if (i<=r)
-		zeta[i]= min(r-i+1,z[i-l]);
+		zeta[i]= min(r-i+1,zeta[i-l]);
 		while(i+zeta[i]<s.size() && s[zeta[i]]==s[i+zeta[i]])
 		zeta[i]++;
 		if (i+zeta[i]-1>r)
