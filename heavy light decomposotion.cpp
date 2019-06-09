da8bc223be64744226e7bb7cf18b20753a703328
Huzaifa <huzaifajazzmankda@gmail.com>
2019-06-09 17:53:02 +0530

    Initial Commit

---
+++ heavy light decomposotion.cpp
@@ -0,0 +1,120 @@
+//Usage heavylight_decomposition h1(mrg)
+//mrg is merge function of segment tree;
+//Don't forget to write calc_sar() method
+//put lca first
+//then put segment tree or other datastructure
+//then continue from below;
+int sbsz[MAX];
+class heavylight_decomposition
+{
+	private:
+	//chid is starting element of chain
+	int chid[MAX],sid[MAX],sar[MAX],itr,root;
+	//chn[i] has all the element of the chain starting at i-th node
+	vector<int> chn[MAX];
+	// Segment tree or any other data structure
+	segment_tree t1;
+	void calc_sar()
+	{
+		/*------------Fill this----------*/
+	}
+	void getsize(int x,int p)
+	{
+		sbsz[x]=1;
+		for(auto u:adjlst[x])
+		if(u!=p)
+		{
+			getsize(u,x);
+			sbsz[x]+=sbsz[u];
+		}
+	}
+	void decompose(int x,int p,int id)
+	{
+		sid[x]=itr++;
+		chn[id].push_back(x);
+		chid[x]=id;
+		int bigc=-1,mx=-1;
+		for(auto u:adjlst[x])
+		{
+			if(u!=p && mx<sbsz[u])
+			{
+				mx=sbsz[u];
+				bigc=u;
+			}
+		}
+		if(bigc!=-1)
+		decompose(bigc,x,id);
+		for(auto u:adjlst[x])
+		{
+			if(u!=p && bigc!=u)
+			decompose(u,x,u);
+		}
+	}
+	//v is ancestor of u
+	int query_up(int u,int v)
+	{
+		//assign proper value
+		int ans;
+		while(u)//make it u!=v for edge calculation stuff
+		{
+			if(chid[u]==chid[v])
+			{
+				//both u and v are in same chain
+				ans=max(ans,t1.query(1,1,n,sid[v],sid[u]));
+				return ans;//remove this for edge calculation stuff
+			}
+			ans=max(ans,t1.query(1,1,n,sid[chid[u]],sid[u]));
+			u=up[chid[u]][0];
+		}
+		return ans;
+	}
+	//v is ancestor of u
+	void update_up(int u,int v,int val)
+	{
+		while(u!=v)
+		{
+			if(chid[u]==chid[v])
+			{
+				//both u and v are in same chain
+				t1.updateRange(1,sar,1,n,sid[v],sid[u],val);
+			}
+			t1.updateRange(1,sar,1,n,sid[chid[u]],sid[u],val);
+			u=up[chid[u]][0];
+		}
+		return;
+	}
+	public:
+	//modify constructor as per data structure
+	heavylight_decomposition(auto mrg,int root=1)
+	{
+		t1=segment_tree(mrg);
+		itr=1;
+		getsize(root,0);
+		pre_lca(root);
+		decompose(root,0,root);
+		// assign values in sar[] to builld Range data structure
+		calc_sar();
+		//Build tree
+		t1.build(1,sar,1,n);
+	}
+	int query(int u,int v)
+	{
+		int luv=lca(u,v);
+		int pu=query_up(u,luv);
+		int pv=query_up(v,luv);
+		return max(pu,pv);
+	}
+	//Point Update 
+	void update(int u,int val)
+	{
+		t1.update(1,sar,1,n,u,val);
+	}
+	//Range Update
+	void updateRange(int u,int v,int val)
+	{
+		int luv=lca(u,v);
+		update_up(u,luv,val);
+		update_up(v,luv,val);
+		return;
+	}
+};
