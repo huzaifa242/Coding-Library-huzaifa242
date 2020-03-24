48dde67db0ad2bb081be90f116904effc1bdfe7f
Huzaifa <huzaifajazzmankda@gmail.com>
2020-03-24 18:15:26 +0530

    refactor

--- policy based data structure.cpp
+++ policy based data structure.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp> 
+#include <ext/pb_ds/detail/standard_policies.hpp>
 using namespace __gnu_pbds;
 typedef tree<
 int,
