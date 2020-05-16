4ba430fecf464679804e3b001c314bf5092d9016
huzaifa242 <huzaifajazzmankda@gmail.com>
2020-05-16 21:19:54 +0530

    improving interactive runner

--- contest_setup/interactiverunner.sh
+++ contest_setup/interactiverunner.sh
@@ -6,15 +6,16 @@ if [[ -z "$2" ]]; then
 	interactiverunner="$TESTING_HOME""/defaultinteractor"
 else
 	interactiverunner="$2"	
-	custominteractor="$interactiverunner" # put extension here
+	ext=".cpp" # put extension here
+	custominteractor="$interactiverunner$ext" 
 fi
 # Compile interactor if required. put correct value of custominteractor
-# g++ -o "${interactiverunner}" "${custominteractor}" -D ONLINE_JUDGE -D LOCAL
+g++ -o "${interactiverunner}" "${custominteractor}" -D ONLINE_JUDGE -D LOCAL
 compile=$((g++ -o "$codefile" "$codefile".cpp -D ONLINE_JUDGE -D LOCAL >&2) 2>&1) 
 echo $directory
 if [[ ${#compile} -le 3 ]]; then
 	# below step is the running command
-	timeout 10 python3 "$CONTEST_SETUP/interactor.py" python3 "$interactiverunner.py" 0 -- "$codefile"
+	timeout 10 python3 "$CONTEST_SETUP/interactor.py"  "$interactiverunner" 0 -- "$codefile"
 	cp "$codefile.cpp" "$HOME/Desktop/submit.cpp"
 else
 	echo "$compile"
