4d02e9d02c6ff4a3386998c691a4eadb131eacc0
huzaifa242 <huzaifajazzmankda@gmail.com>
2020-06-28 19:34:28 +0530

    spacing changes

--- persistent tree.cpp
+++ persistent tree.cpp
@@ -2,16 +2,16 @@
  * When using for Tree label vertices in order of visit
  * Don't just depend on parent it gives WA
  */
-struct data {
-	data *left, *right;
+struct info {
+	info *left, *right;
 	int val;
-	data(int v) {
+	info(int v) {
 		left = nullptr;
 		right = nullptr;
 		//inits
 		val = v;
 	}
-	data(data* l, data* r) {
+	info(info* l, info* r) {
 		val = 0;
 		//merge logic
 		if (l != nullptr) {
@@ -26,29 +26,29 @@ struct data {
 };
 class persistent_tree {
 	private:
-	vector <data*> roots;
-	data* build(int a[], int l, int r) {
+	vector <info*> roots;
+	info* build(int a[], int l, int r) {
 		if (l > r)
 			return nullptr;
 		if (l == r)
-			return new data(a[l]);
+			return new info(a[l]);
 		int m = (l + r) / 2;
-		return new data(build(a, l, m), build(a, m + 1, r));
+		return new info(build(a, l, m), build(a, m + 1, r));
 	}
-	data* update(data* node, int l, int r, int id, int z) {
+	info* update(info* node, int l, int r, int id, int z) {
 		if (l > r)
 			return nullptr;
 		if (l == r) {
 			//Do correct update here
-			return new data(z);
+			return new info(z);
 		}
 		int m = (l + r) / 2;
 		if (id <= m)
-			return new data(update(node->left, l, m, id, z), node->right);
+			return new info(update(node->left, l, m, id, z), node->right);
 		else
-			return new data(node->left, update(node->right, m + 1, r, id, z));
+			return new info(node->left, update(node->right, m + 1, r, id, z));
 	}
-	data* query(data* node, int l, int r, int x, int y) {
+	info* query(info* node, int l, int r, int x, int y) {
 		if (l > r || x > r || y < l)
 			return nullptr;
 		if (l >= x && r <= y)
@@ -59,7 +59,7 @@ class persistent_tree {
 		else if (m < x)
 			return query(node->right, m + 1, r, x, y);
 		else
-			return new data(query(node->left, l, m, x, y), query(node->right, m + 1, r, x, y));
+			return new info(query(node->left, l, m, x, y), query(node->right, m + 1, r, x, y));
 	}
 	public:
 	persistent_tree(){
@@ -70,9 +70,9 @@ class persistent_tree {
 			return;
 		roots.push_back(update(roots[rt], l, r, id, z));
 	}
-	data query(int rt, int l, int r, int x, int y) {
+	info query(int rt, int l, int r, int x, int y) {
 		if (rt >= roots.size()) {
-			data grbg;//set garbage
+			info grbg;//set garbage
 			return grbg;
 		}
 		return *(query(roots[rt], l, r, x, y));
