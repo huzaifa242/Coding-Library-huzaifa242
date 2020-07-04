58a92c954898acdf95c475197c3eb9acfc9f738a
huzaifa242 <huzaifajazzmankda@gmail.com>
2020-07-04 18:09:51 +0530

    templatizing persistent tree

--- persistent tree.cpp
+++ persistent tree.cpp
@@ -24,31 +24,32 @@ struct info {
 		}
 	}
 };
+template <typename T>
 class persistent_tree {
 	private:
-	vector <info*> roots;
-	info* build(int a[], int l, int r) {
+	vector <T*> roots;
+	T* build(int a[], int l, int r) {
 		if (l > r)
 			return nullptr;
 		if (l == r)
-			return new info(a[l]);
+			return new T(a[l]);
 		int m = (l + r) / 2;
-		return new info(build(a, l, m), build(a, m + 1, r));
+		return new T(build(a, l, m), build(a, m + 1, r));
 	}
-	info* update(info* node, int l, int r, int id, int z) {
+	T* update(T* node, int l, int r, int id, int z) {
 		if (l > r)
 			return nullptr;
 		if (l == r) {
 			//Do correct update here
-			return new info(z);
+			return new T(z);
 		}
 		int m = (l + r) / 2;
 		if (id <= m)
-			return new info(update(node->left, l, m, id, z), node->right);
+			return new T(update(node->left, l, m, id, z), node->right);
 		else
-			return new info(node->left, update(node->right, m + 1, r, id, z));
+			return new T(node->left, update(node->right, m + 1, r, id, z));
 	}
-	info* query(info* node, int l, int r, int x, int y) {
+	T* query(T* node, int l, int r, int x, int y) {
 		if (l > r || x > r || y < l)
 			return nullptr;
 		if (l >= x && r <= y)
@@ -59,7 +60,7 @@ class persistent_tree {
 		else if (m < x)
 			return query(node->right, m + 1, r, x, y);
 		else
-			return new info(query(node->left, l, m, x, y), query(node->right, m + 1, r, x, y));
+			return new T(query(node->left, l, m, x, y), query(node->right, m + 1, r, x, y));
 	}
 	public:
 	persistent_tree(){
@@ -70,9 +71,9 @@ class persistent_tree {
 			return;
 		roots.push_back(update(roots[rt], l, r, id, z));
 	}
-	info query(int rt, int l, int r, int x, int y) {
+	T query(int rt, int l, int r, int x, int y) {
 		if (rt >= roots.size()) {
-			info grbg;//set garbage
+			T grbg;//set garbage
 			return grbg;
 		}
 		return *(query(roots[rt], l, r, x, y));
